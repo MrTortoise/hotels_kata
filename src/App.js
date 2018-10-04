c7ac02b8a660f2fd45c1675b2aaa4bf0a4d0c1be
John Nicholas <themrtortoise@gmail.com>
2018-10-04 12:01:46 +0100

    hotel search

--- src/App.js
+++ src/App.js
@@ -3,10 +3,5 @@ import logo from './logo.svg';
 import './App.css';
 
 export default () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-    </header>
-    <div>App</div>
-  </div>
+    <div className="hotel-search">App</div>
 );
